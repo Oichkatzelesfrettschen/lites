f332519ac668f8b75235935463aa425b075bb441
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-25 16:28:16 -0700

    Merge branch 'master' of https://Oichkatzelesfrettschen@github.com/Oichkatzelesfrettschen/lites
    Historical versions for reference and and compare for ml analysis.

---
+++ include/keystore.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stddef.h>
+
+int ks_generate_key(const char *path, size_t len);
+int ks_encrypt(const char *key_path, const unsigned char *in, size_t in_len, unsigned char *out,
+               size_t *out_len);
+int ks_decrypt(const char *key_path, const unsigned char *in, size_t in_len, unsigned char *out,
+               size_t *out_len);
+
